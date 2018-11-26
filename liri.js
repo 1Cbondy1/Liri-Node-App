8d5843ccc7625dfbb2029f55d0d137b1a5124899
1Cbondy1 <claytonbondy@gmail.com>
2018-11-25 18:10:08 -0600

    added to concert-this function

--- liri.js
+++ liri.js
@@ -19,27 +19,27 @@ else {
 
 function concertThis() {
 
-    if (process.argv[3]) {
-
+    if (process.argv[3]) { 
         var movieName = process.argv[3];
-        var movieUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
-
-        // Runs a request with axios to the OMDB API with the movie specified
-        axios.get(movieUrl).then(
-            function(response) {
-                console.log("\n" + "Title: " + response.data.Title);
-                console.log("Year: " + response.data.Released);
-                console.log("IMDB Rating: " + response.data.imdbRating + "\n");
-                // console.log("Rotten Tomatoes Rating: " + response.data.);
-                // console.log("Country: " + response.data.);
-                // console.log("Language: " + response.data.);
-                // console.log("Actors: " + response.data.);
-            }
-        );
-
-    } else {
-        console.log("Please enter a movie to search.");
     }
+    else {
+        var movieName = "Mr. Nobody";
+    }
+
+    var movieUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
+
+    // Runs a request with axios to the OMDB API with the movie specified
+    axios.get(movieUrl).then(
+        function(response) {
+            console.log("\n" + "Title: " + response.data.Title);
+            console.log("Year: " + response.data.Released);
+            console.log("IMDB Rating: " + response.data.imdbRating);
+            console.log("Rotten Tomatoes Rating: " + response.data.Ratings[1].Value);
+            console.log("Country: " + response.data.Country);
+            console.log("Language: " + response.data.Language);
+            console.log("Actors: " + response.data.Actors + "\n");
+        }
+    );
 }
 
 function validCommand() {
